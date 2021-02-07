1b076105ff7183cc9352cad44d32ab5a07cdfb65
pierrewenzel17 <pierre.wenzel@outlook.com>
2021-02-07 14:14:06 +0100

    Test compte

--- Client/src/client/__main__.py
+++ Client/src/client/__main__.py
@@ -6,7 +6,7 @@ from client.controllers import MainController
 if __name__ == '__main__':
     root = Tk()
     root.title("Square Assembler")
-    root.resizable(width=False, height=False)
+    root.resizable(width=True, height=True)
     root.minsize(1200, 900)
     controller_main = MainController(root)
     controller_main.run()
