ea23f942052920cdf5b4ca436a471609919cf6d0
cyril <cyrilburtin@hotmail.fr>
2021-04-16 19:00:27 +0200

    pierre fille

--- Client/src/client/models/serveur.py
+++ Client/src/client/models/serveur.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from time import sleep
 
 from ivy.ivy import IvyServer
@@ -6,32 +7,20 @@ from core.models.grid import Grid
 from core.models.position import Position
 
 
+class StateOnline(Enum):
+    CREATE = True
+    JOIN = False
+
+    def __eq__(self, o) -> bool:
+        if isinstance(o, StateOnline):
+            return self.name == o.name
+        return False
+
+
 class MyAgent(IvyServer):
 
-    def __init__(self, agent_name: str, is_client_to_send: bool) -> None:
-        IvyServer.__init__(self, agent_name)
-        self.start()
-        self.is_client_to_send = is_client_to_send
-        self.bind_msg(self.send_grid, "(grid)")
-        self.bind_msg(self.send_grid_modification, "(position)")
-
-    def send_grid(self, agent: 'MyAgent', grid: str):
-        """
-        fonction qui envoie la grille de départ à l'autre joueur
-        :param grid: la grille initialiser sans modification
-        :return: True si l'envoie c'est bien passer false sinon
-        """
-        if self.is_client_to_send:
-            return Grid.rebulde(grid)
-        else:
-            return None
-
-    def send_grid_modification(self, agent: 'MyAgent', position: str) -> Position:
-        """
-        :param position:
-        :return:
-        """
-        return Position.rebuld(position)
+    def __init__(self, agent_name: str, state) -> None:
+        IvyServer.__init__(self, agent_name, f'connect={state.name}')
 
     def __str__(self) -> str:
         return super().__str__()
@@ -39,14 +28,15 @@ class MyAgent(IvyServer):
 
 if __name__ == '__main__':
     print('Ici on test le serveur')
-    server = MyAgent("server", False)
+    server = MyAgent("server")
     sleep(2)
-    client = MyAgent("client", True)
+    server2 = MyAgent("client")
     sleep(2)
-    teste=Grid.grid_by_ten()
-
+    server.send_msg(f'connect=({StateOnline.CREATE.name})')
+    '''
+    teste = Grid.grid_by_ten()
     server.send_msg(teste.__str__())
-    pter=Position(1,2)
+    pter = Position(1, 2)
     client.send_msg(pter.__str__())
-    sleep(3)
+    sleep(3)'''
     print("faim")
